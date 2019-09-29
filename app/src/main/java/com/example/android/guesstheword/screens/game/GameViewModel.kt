af0cc883629fb55a029ec05db2a09bc03dbcd9d2
Sunny <trinnakrit@gmail.com>
2019-09-29 17:35:24 +0700

    Task: Add transformation for the LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 
@@ -21,11 +23,16 @@ class GameViewModel : ViewModel() {
 
     }
 
+
     private val _currentTime = MutableLiveData<Long>()
     val currentTime: LiveData<Long>
         get() = _currentTime
+
     private val timer: CountDownTimer
     // The current word
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
